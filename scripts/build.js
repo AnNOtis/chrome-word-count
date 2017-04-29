08552fdd1556a3e2d05b49468649acf95a4ec471
Otis Chou <altispec@gmail.com>
2017-04-29 19:20:26 +0800

    build script

---
+++ scripts/build.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+const Zip = require('node-zip')
+
+const name = 'word-counts'
+
+const zip = new Zip()
+
+const files = [
+  'icon-128.png',
+  'main.js',
+  'manifest.json'
+]
+files.forEach(file => {
+  zip.folder(name).file(file, fs.readFileSync(path.join(__dirname, '../', file)))
+})
+
+zip.folder(name).file('key.pem', fs.readFileSync(path.join(__dirname, '../', 'chrome-word-count.pem')))
+
+const data = zip.generate({base64: false, compression: 'DEFLATE'})
+fs.writeFileSync(`build/${name}.zip`, data, 'binary')
